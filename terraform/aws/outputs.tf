705a7fac0d6b5ea68d5abe52d57f57959f4b79f3
lahiruj <lahirujayathilake@gmail.com>
2024-03-28 13:02:06 -0400

    tf scripts for AWS Keycloak infrastructure

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -13,3 +13,40 @@ output "state_table" {
 output "state_bucket" {
   value = module.terraform_state_backend.s3_bucket_domain_name
 }
+
+output "alb_dns_name" {
+  value = module.keycloak.alb_dns_name
+}
+
+output "alb_log_bucket" {
+  value = module.keycloak.alb_log_bucket
+}
+
+output "ecr_repo" {
+  value = module.keycloak.ecr_repo
+}
+
+output "ecs_cluster" {
+  value = module.keycloak.ecs_cluster
+}
+
+output "ecs_service" {
+  value = module.keycloak.ecs_service
+}
+
+output "rds_cluster_endpoint" {
+  value = module.keycloak.rds_cluster_endpoint
+}
+
+output "rds_cluster_reader_endpoint" {
+  value = module.keycloak.rds_cluster_reader_endpoint
+}
+
+output "rds_cluster_database_name" {
+  value = module.keycloak.rds_cluster_database_name
+}
+
+output "rds_cluster_master_username" {
+  value     = module.keycloak.rds_cluster_master_username
+  sensitive = true
+}
