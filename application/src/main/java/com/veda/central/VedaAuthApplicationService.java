acfd19a4dbf5aa39caefc506b4882a9bf5a8e434
lahiruj <lahirujayathilake@gmail.com>
2024-04-09 18:42:33 -0400

    initial project structure and integration of core dependencies

---
+++ application/src/main/java/com/veda/central/VedaAuthApplicationService.java
@@ -0,0 +1,11 @@
+package com.veda.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedaAuthApplicationService {
+    public static void main(String[] args) {
+        SpringApplication.run(VedaAuthApplicationService.class, args);
+    }
+}
